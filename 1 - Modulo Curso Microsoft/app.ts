5a8e88b19e1ee620ce27ee1e45b71ccf773b015a
flavioepimentel <flavio.ericksan@hotmail.com>
2022-04-01 09:36:45 -0300

    Formata arquivo de consulta

--- 1 - Modulo Curso Microsoft/app.ts
+++ 1 - Modulo Curso Microsoft/app.ts
@@ -69,8 +69,33 @@ let arrayNumbers:number[]= new Array(1,2,3,4,5,6)
 function pecorrendoListas(arrayNumbers:number[]) {
     for (let i = 0; i < arrayNumbers.length; i++) {
         console.log(arrayNumbers[i]);
-        
     }    
 }
 
 pecorrendoListas(arrayNumbers)
+
+// Tuplas
+let firstTupla: [string, string, number, number];
+firstTupla = ['uma string', 'duas strings', 1, 2]
+console.log(firstTupla)
+
+// Tuplas com labels
+let labelTupla: [nome:string, idade:number, trabalho:string] = ['User', 30, 'Desenvolvedor']
+console.log(labelTupla[0])
+
+// Tuplas homogêneas com Spread Operator
+let listaFrutas:[string, ...string[]] = ['laranja', 'maçã', 'pessego', 'ameixa']
+console.log(...listaFrutas)
+
+// Lista Heterogênea
+let listaFrutas2:[number, boolean, ...string[]] = [8, true, ...listaFrutas]
+console.log(...listaFrutas2)
+
+// Tuplas em funções
+function funcTupla (nomes:string[], idades:number[]){
+    return [...nomes, ...idades]
+}
+
+let response = funcTupla(['user1', 'user2'],[25, 45])
+console.log(response)
+
